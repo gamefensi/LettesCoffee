edb556c27788904ddd83abf831825b3136ce9cf9
John M <mahan.john@gmail.com>
2022-02-28 21:59:14 -0800

    fixing more errors in webpack

--- node-mongo/webpack.config.js
+++ node-mongo/webpack.config.js
@@ -1,27 +1,36 @@
 const webpack = require('webpack');
 const path = require('path');
+const { IgnorePlugin } = require('webpack');
+
+const optionalPlugins = [];
+if (process.platform !== "darwin") {
+  optionalPlugins.push(new IgnorePlugin({ resourceRegExp: /^fsevents$/ }));
+}
+const nodeExternals = require('webpack-node-externals');
+
 
 const config = {
-    resolve: {
-      modules: ['node_modules'],
-      fallback: {
-        "fs": false,
-        "tls": false,
-        "net": false,
-        "path": false,
-        "zlib": false,
-        "http": false,
-        "https": false,
-        "stream": false,
-        "crypto": false,
-        "child_process": false,
-        "os": false,
-        "crypto": false,
-        "url": false,
-        "util": false,
-        "querystring": false,
-      } 
-    },
+  resolve: {
+    modules: ['node_modules'],
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "child_process": false,
+      "os": false,
+      "crypto": false,
+      "url": false,
+      "util": false,
+      "querystring": false,
+      "events": false
+    }
+  },
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -35,7 +44,12 @@ const config = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  plugins: [
+    ...optionalPlugins,
+  ]
 };
 
 module.exports = config;
