1d7c27242bb760fde98f65040f9e0d425644dc55
John M <mahan.john@gmail.com>
2022-03-05 22:14:05 -0800

    form validation

--- mern-lettes-coffee/src/Register.js
+++ mern-lettes-coffee/src/Register.js
@@ -11,22 +11,71 @@ const Register = (props) => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [userContext, setUserContext] = useContext(UserContext)
-  const [validated, setValidated] = useState(false);
+  // const [validated, setValidated] = useState(false);
+  const [form, setForm] = useState({})
+  const [errors, setErrors] = useState({})
+
+  const setField = (field, value) => {
+    setForm({
+      ...form,
+      [field]: value
+    })
+    //Check and see if errros exist, and remove them from the error object
+    if ( !!errors[field]) setErrors({
+      ...errors,
+      [field]: null
+    })
+  }
+  
+  const findFormErrors = () => {
+    const { firstName, lastName, email, password } = form
+    const newErrors = {}
+    // name errors
+    if ( !firstName || firstName === '' ) newErrors.firstName = 'cannot be blank!'
+    else if ( firstName.length > 30 ) newErrors.firstName = 'name is too long!'
+    // food errors
+    if ( !lastName || lastName === '' ) newErrors.lastName = 'cannot be blank!'
+    else if ( lastName.length > 30 ) newErrors.lastName = 'name is too long!'
+    // rating errors
+    if ( !email || email === '' ) newErrors.email = 'cannot be blank!'
+    else if (email.match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/)) newErrors.email = 'please enter a valid email address'
+    // comment errors
+    if ( !password || password === '' ) newErrors.password = 'cannot be blank!'
+    else if (password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/)) newErrors.password = 'password must be 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter'
+
+    return newErrors
+}
 
   const formSubmitHandler = e => {
     e.preventDefault()
     setIsSubmitting(true)
-    setError("")
+    const newErrors = findFormErrors()
 
+//     const inputs = document.querySelectorAll('input');
 
-    const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-    setValidated(true);
+// const patterns = {
+//   password: /^[\d\w@-]{8,20}$/i,
+//   email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+//   phone: /^\d{3}-\d{3}-\d{4}$/
+// };
 
+// inputs.forEach((input) => {
+//   input.addEventListener('keyup', (e) => {
+//     validate(e.target, patterns[e.target.attributes.id.value]);
+//   });
+// });
 
+// function validate(field, regex) {
+//   if (regex.test(field.value)) {
+//     setValidated(true);
+//   } else {
+//     setValidated(false);
+//   }
+// }
+    if ( Object.keys(newErrors).length > 0 ) {
+      setIsSubmitting(false)
+      setErrors(newErrors)
+    } else {
 
     const genericErrorMessage = "Something went wrong! Please try again later."
 
@@ -63,64 +112,74 @@ const Register = (props) => {
         setIsSubmitting(false)
         setError(genericErrorMessage)
       })
-
+    }
   };
 
   return (
     <>
-      <form onSubmit={formSubmitHandler} className="auth-form" validated={validated} noValidate>
+      {error && <Alert intent="danger">{error}</Alert>}
+      <form onSubmit={formSubmitHandler} className="auth-form">
         <Form.Group controlId="validationCustom01">
           <Form.Label>First Name</Form.Label>
           <Form.Control
-            id="firstName"
+            // id="firstName"
+            type='text'
             required
-            onChange={e => setFirstName(e.target.value)}
-            value={firstName}
+            onChange={e => setField('name', e.target.value)}
+            // value={firstName}
+            isInvalid={ !!errors.firstName}
           />
           <Form.Control.Feedback type="invalid">
-            Please enter first name.
+            { errors.name }
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="validationCustom0w">
           <Form.Label>Last Name</Form.Label>
           <Form.Control
-            id="lastName"
+            // id="lastName"
+            type='text'
             required
-            onChange={e => setLastName(e.target.value)}
-            value={lastName}
+            onChange={e => setField('name', e.target.value)}
+            // value={lastName}
+            isInvalid={ !!errors.lastName}
           />
           <Form.Control.Feedback type="invalid">
-            Please enter last name.
+            { errors.name }
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="validationCustom03">
           <Form.Label>Email</Form.Label>
           <Form.Control
-            id="email"
+            // id="email"
+            type='text'
             required
-            onChange={e => setEmail(e.target.value)}
-            value={email}
+            onChange={e => setField('name', e.target.value)}
+            // value={email}
+            isInvalid={ !!errors.email}
           />
           <Form.Control.Feedback type="invalid">
-            Please enter a valid email.
+            { errors.name }
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="validationCustom04">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            id="password"
+            // id="password"
+            type='text'
             required
-            onChange={e => setPassword(e.target.value)}
-            value={password}
+            onChange={e => setField('name', e.target.value)}
+            // value={password}
+            isInvalid={ !!errors.password}
           />
           <Form.Control.Feedback type="invalid">
-            Please enter a password.
+            { errors.name }
           </Form.Control.Feedback>
         </Form.Group>
         <Button
           variant="primary"
           disabled={isSubmitting}
           type="submit"
+          className="mt-3"
         >
           {`${isSubmitting ? "Registering" : "Register"}`}
         </Button>
