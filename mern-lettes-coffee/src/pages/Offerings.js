0dd1c846c803cf95da73e3cc8f7aae9d026c9383
John M <mahan.john@gmail.com>
2022-03-07 00:38:56 -0800

    Removed quantity/weight selection on offerings page

--- mern-lettes-coffee/src/pages/Offerings.js
+++ mern-lettes-coffee/src/pages/Offerings.js
@@ -60,7 +60,7 @@ export default function Offerings(props) {
         {Coffee.map((item, index) => (
           <ListGroupItem key={item.id}>
             <Row style={{
-              margin: 'auto'
+              margin: 'auto',
             }} >
               <Col xs="4" lg="5" className="product-wrapper">
                 <img
@@ -72,15 +72,15 @@ export default function Offerings(props) {
               </Col>
               <Col xs="12" lg="4" className="coffee-info-xl my-xs-3 my-xl-1">
                 <ul>
-                  <li><h3>{item.name}</h3></li>
-                  <li><strong>Price: </strong><span className="item-price">
+                  <li className='mb-4'><h3>{item.name}</h3></li>
+                  {/* <li><strong>Price: </strong><span className="item-price">
                     {
                       item.weight === "0" ? 'select a weight' :
                         item.weight === "12" ? '$'+item.price12 :
                           '$'+item.price24
                     }</span>
-                  </li>
-                  <li><strong>Country of Origin:</strong> {item.country}</li>
+                  </li> */}
+                  <li className='mb-4'><strong>Country of Origin:</strong> {item.country}</li>
                   {/* // 1.) Modal Option:
                   <li><Button className="my-lg-4 my-xs-2" variant="outline-light" onClick={() => handleShow2(item)}><strong>flavor profile</strong></Button></li> */}
                   {/* // 2.) Popover option */}
@@ -99,16 +99,16 @@ export default function Offerings(props) {
               </Col>
               <Col className="orderForm" xs="12" lg="3">
                 <Form>
-                  <FormGroup>
+                  {/* <FormGroup>
                     <Label for="weight" style={{ color: '#f5deb3' }} className="pt-4">Weight:</Label>
                     <Form.Select name="weight" aria-label="select weight" onChange={props.handleWeight(index)}>
                       <option value="0">Select an option</option>
                       <option value="12">12oz</option>
                       <option value="24">24oz</option>
                     </Form.Select>
-                  </FormGroup>
+                  </FormGroup> */}
                   <Row className="mt-4">
-                    <Col xl="6">
+                    {/* <Col xl="6">
                       <Label for="qty" style={{ color: '#f5deb3' }} >Quantity:</Label>
                       <Input
                         name="qty"
@@ -116,14 +116,15 @@ export default function Offerings(props) {
                         defaultValue={item.cartQty}
                         style={{ marginBottom: "20px" }}
                         onChange={props.handleCartQty(index,'+')} />
-                    </Col>
-                    <Col xl="6" >
-                      <LinkContainer to="/Schedule" className="my-xs-4">
+                    </Col> */}
+                    <strong style={{color:"#fff",textTransform:"uppercase",fontSize:"larger",display:"inline"}}>Price: </strong>
+                    <span className="item-price" style={{display:"inline",color:"#fff"}}>${item.price12} (12oz) / ${item.price24} (24oz)</span>
+                      <LinkContainer to="/Schedule" >
                       <Button
                         variant="outline-light"
                         className="addToCart"
                         type="button"
-                        size="sm"
+                        size="lg"
                         // onClick={() =>
                         //   (item.weight === "0") 
                         //   ? showAlert() : props.addToCart(item, item.cartQty, item.selectedWeight, 0)}
@@ -131,7 +132,6 @@ export default function Offerings(props) {
                         View Roast Schedule
                       </Button>
                       </LinkContainer>
-                    </Col>
                   </Row>
                 </Form>
               </Col>
@@ -158,32 +158,34 @@ export default function Offerings(props) {
               <ListGroup variant="flush">
                 {/* <ListGroupItem><h3>{item.name}</h3></ListGroupItem> */}
                 <ListGroupItem><strong>Price: </strong><span className="item-price">
-                    {
+                    {/* {
                       item.weight === "0" ? 'select a weight' :
                         item.weight === "12" ? '$'+item.price12 :
                           item.price24
-                    }</span></ListGroupItem>
+                    } */}
+                    ${item.price12} (12oz) / ${item.price24} (24oz)
+                    </span></ListGroupItem>
                 <ListGroupItem><strong>Country of Origin:</strong> {item.country}</ListGroupItem>
               </ListGroup>
               <Card.Body>
-                <FormGroup>
+                {/* <FormGroup>
                   <Label for="weight" className="pt-4"><strong>Weight:</strong></Label>
                   <Form.Select name="weight" aria-label="select weight" onChange={props.handleWeight(index)}>
                     <option value="0">Select an option</option>
                     <option value="12">12oz</option>
                     <option value="24">24oz</option>
                   </Form.Select>
-                </FormGroup>
+                </FormGroup> */}
                 <Row className="mt-4">
-                  <Col xs="12" md="5">
+                  {/* <Col xs="12" md="5">
                     <Label for="qty" ><strong>Quantity:</strong></Label>
                     <Input
                       name="qty"
                       type="number"
                       defaultValue={item.cartQty}
                       onChange={props.handleCartQty(index,'+')} />
-                  </Col>
-                  <Col xs="12" md="7">
+                  </Col> */}
+                  <Col xs="12">
                     <LinkContainer to="/Schedule">
                     <Button
                       variant="outline-dark"
